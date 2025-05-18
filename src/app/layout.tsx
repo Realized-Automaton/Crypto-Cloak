515982522a498ab6d97ad449b6461f1ce8d8f483
Realized-Automaton <silverlinings905@yahoo.com>
2025-05-18 09:29:25 -0400

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body
         className={cn(
           geistSans.variable,
